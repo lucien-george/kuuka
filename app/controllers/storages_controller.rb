68a7b7a9d40608a5f1b091bee707d9a899397c9f
Lucien George <luciengeorge95@gmail.com>
2019-03-20 11:05:28 +0000

    storage controller done

--- app/controllers/storages_controller.rb
+++ app/controllers/storages_controller.rb
@@ -1,2 +1,50 @@
 class StoragesController < ApplicationController
+  before_action :find_storage, only: %i[show update edit destroy]
+
+  def index
+    @storages = Storage.all
+  end
+
+  def show
+  end
+
+  def new
+    @storage = Storage.new
+  end
+
+  def create
+    @storage = Storage.new(storage_params)
+    if @storage.save
+      redirect_to storage_path(@storage)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @storage.update(storage_params)
+    if @storage.save
+      redirect_to storage_path(@storage)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @storage.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_storage
+    @storage = Storage.find(params[:id])
+  end
+
+  def storage_params
+    params.require(:storage).permit(:description, :size)
+  end
 end
