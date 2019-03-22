f55b8ff048e16b759ca601dbf33064a35ba6bc79
Lucien George <luciengeorge95@gmail.com>
2019-03-22 12:35:36 +0000

    form done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true # , uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   # validates :age, presence: true, numericality: { only_integer: true }
