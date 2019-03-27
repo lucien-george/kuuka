b44d0ff1212e425062e40e104c7a8bc8aa38d1f2
Lucien George <luciengeorge95@gmail.com>
2019-03-27 11:23:56 +0000

    mailer layout done

--- test/mailers/previews/storage_mailer_preview.rb
+++ test/mailers/previews/storage_mailer_preview.rb
@@ -3,7 +3,7 @@ class StorageMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/storage_mailer/new_storage_created
   def new_storage_created
-    StorageMailer.new_storage_created
+    StorageMailer.new_storage_created(Storage.last, User.last)
   end
 
 end
